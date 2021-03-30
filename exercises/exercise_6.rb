3b191a011c97dd9bf310c4bcedb8176ec99f708e
ermurray <ermurray31@gmail.com>
2021-03-30 04:51:24 +0000

    add to class store has_many and to employee belongs_to, added three employees to stor1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Dirt", hourly_rate: 10)
+@store1.employees.create(first_name: "Christian", last_name: "Horner", hourly_rate: 600)
+
+@store2.employees.create(first_name: "Sebastian", last_name: "Vettle", hourly_rate: 1000)
+@store2.employees.create(first_name: "Lewis", last_name: "Hamilton", hourly_rate: 6000)
+@store2.employees.create(first_name: "Daniel", last_name: "Riccardo", hourly_rate: 3000)
+
